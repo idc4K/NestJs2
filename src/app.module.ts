5f4f825acd010664893c45ce8dd8d9e456cc8e83
idc4k <christikonga83@gmail.com>
2023-03-28 11:05:08 +0000

    resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PetsModule } from './pets/pets.module';
-
+import {ApolloDriver, ApolloDriverConfig} from '@nestjs/apollo'
 @Module({
-  imports: [GraphQLModule.forRoot({
+  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
     autoSchemaFile : join(process.cwd(), 'src/schema.gql'),
+    driver : ApolloDriver
   }) , PetsModule],
   controllers: [AppController],
   providers: [AppService],
--- src/pets/pets.module.ts
+++ src/pets/pets.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PetsService } from './pets.service';
+import { PetsResolver } from './pets.resolver';
 
 @Module({
-  providers: [PetsService]
+  providers: [PetsService, PetsResolver]
 })
 export class PetsModule {}
